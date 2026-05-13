dbd4065dd96b14a18160323a1ff0a57dbc99ad15
anthonystepvoy <anthonystepvoy@gmail.com>
2026-05-13 12:27:33 -0300

    Fix Linux clippy color init lint

--- src/console.rs
+++ src/console.rs
@@ -1,11 +1,10 @@
 pub fn init(no_color: bool) {
     if no_color {
         colored::control::set_override(false);
-        return;
-    }
-
-    #[cfg(windows)]
-    {
-        let _ = colored::control::set_virtual_terminal(true);
+    } else {
+        #[cfg(windows)]
+        {
+            let _ = colored::control::set_virtual_terminal(true);
+        }
     }
 }
